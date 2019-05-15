5540c1fac23706d6f532b7a1c2e150d73bdc715f
KUSSY <kushida.teppei@dcf.co.jp>
2019-05-15 18:09:39 +0900

    EditControllerに少し誘導を付ける

--- RoomReservationSystem/src/main/java/jp/co/dcf/rrs/application/controller/ReservationEditController.java
+++ RoomReservationSystem/src/main/java/jp/co/dcf/rrs/application/controller/ReservationEditController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,8 +18,14 @@ public class ReservationEditController {
 	@Autowired
 	ReservationListApplicationService reservationListApplicationService;
 
+	@RequestMapping(value = { "/new" })
+	public ModelAndView newReservation(ModelAndView mav, @RequestParam("date") Optional<String> reservationId) {
+		mav.setViewName("reservation_edit");
+		return mav;
+	}
+	
 	@RequestMapping(value = { "/{reservation_id}" })
-	public ModelAndView index(ModelAndView mav, @RequestParam("reservation_id") Optional<String> reservationId) {
+	public ModelAndView edit(ModelAndView mav, @PathVariable("reservation_id") Optional<Integer> reservationId) {
 		mav.addObject("reservationId", reservationId);
 		mav.setViewName("reservation_edit");
 		return mav;
