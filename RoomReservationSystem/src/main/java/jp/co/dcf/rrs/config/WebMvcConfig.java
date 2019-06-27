f5d3966334c04ba205b3f6722a58f6dd6a83f660
KUSSY <kushida.teppei@dcf.co.jp>
2019-06-27 18:50:52 +0900

    メッセージをymlファイルから読めるように

--- RoomReservationSystem/src/main/java/jp/co/dcf/rrs/config/WebMvcConfig.java
+++ RoomReservationSystem/src/main/java/jp/co/dcf/rrs/config/WebMvcConfig.java
@@ -3,15 +3,20 @@ package jp.co.dcf.rrs.config;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.extern.log4j.Log4j2;
+
 @Configuration
+@Log4j2
 public class WebMvcConfig implements WebMvcConfigurer {
 	// propertiesファイル読み込み
    @Bean(name = "messagesProperties")
@@ -26,5 +31,21 @@ public class WebMvcConfig implements WebMvcConfigurer {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setCommonMessages(yamlProperties());
         return messageSource;
+    }
+    
+	@Bean
+    public LocalValidatorFactoryBean validator() throws IOException {
+        LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+        localValidatorFactoryBean.setValidationMessageSource(messageSource());
+        return localValidatorFactoryBean;
+    }
+    @Override
+    public org.springframework.validation.Validator getValidator(){
+		try {
+			return validator();
+		} catch (IOException e) {
+			log.error(e);
+			return null;
+		}
     }
 }
