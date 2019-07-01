918b7b3f18243b057b085115789de28198c0f336
KUSSY <kushida.teppei@dcf.co.jp>
2019-07-01 11:09:06 +0900

    ymlファイル、ReservationのID

--- RoomReservationSystem/src/main/java/jp/co/dcf/rrs/model/Reservation.java
+++ RoomReservationSystem/src/main/java/jp/co/dcf/rrs/model/Reservation.java
@@ -19,7 +19,7 @@ public class Reservation {
 	 * 主キー
 	 */
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Getter
 	@Setter
 	private Integer id;
